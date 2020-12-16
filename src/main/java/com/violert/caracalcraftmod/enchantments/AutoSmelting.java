c3c8c7076d0d8da6125a5fb4d7adaa256cb1719e
jayp-png <60831681+jayp-png@users.noreply.github.com>
2020-12-15 22:13:38 -0800

    oh god

--- src/main/java/com/violert/caracalcraftmod/enchantments/AutoSmelting.java
+++ src/main/java/com/violert/caracalcraftmod/enchantments/AutoSmelting.java
@@ -10,34 +10,41 @@ import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.enchantment.Enchantment.Rarity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod.EventBusSubscriber(modid=Reference.MOD_ID)
-public class Ablaze extends EnchantmentBase {
+public class AutoSmelting extends EnchantmentBase {
 
-	public Ablaze(String name, Rarity rarity, EnumEnchantmentType type, EntityEquipmentSlot[] equipmentSlots) {
+	public AutoSmelting(String name, Rarity rarity, EnumEnchantmentType type, EntityEquipmentSlot[] equipmentSlots) {
 		
 		super(name, rarity, type, equipmentSlots);
 		
 	}
 	
 	@SubscribeEvent
-	public static void effectAblaze(LivingUpdateEvent event) {
+	public static void effectAutoSmelting(BlockEvent.HarvestDropsEvent breakEvent) {
 		
-		EntityLivingBase player = event.getEntityLiving();
-		int level = EnchantmentHelper.getMaxEnchantmentLevel(ModEnchantments.ABLAZE, player);
+		EntityLivingBase player = breakEvent.getHarvester();
+		Block brokenBlock = breakEvent.getState().getBlock();
+		int level = EnchantmentHelper.getMaxEnchantmentLevel(ModEnchantments.AUTO_SMELTING, player);
 		BlockPos pos = player.getPosition();
-		World world = event.getEntity().world;
+		World world = player.world;
 		
-		if(player.isBurning() && level > 0) {
-			player.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, (10), level-1, false, false));
+		if(level > 0) {
+			String brokenBlockName = brokenBlock.getRegistryName().toString();
+			System.out.println(brokenBlockName);
 		}
 		
 	}
--- src/main/java/com/violert/caracalcraftmod/init/ModEnchantments.java
+++ src/main/java/com/violert/caracalcraftmod/init/ModEnchantments.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.violert.caracalcraftmod.enchantments.Ablaze;
+import com.violert.caracalcraftmod.enchantments.AutoSmelting;
 import com.violert.caracalcraftmod.enchantments.EnchantmentBase;
 import com.violert.caracalcraftmod.enchantments.SandStrider;
 
@@ -18,5 +19,6 @@ public class ModEnchantments {
 	
 	public static final Enchantment SAND_STRIDER = new SandStrider("sand_strider", Rarity.COMMON, EnumEnchantmentType.ARMOR_FEET, new EntityEquipmentSlot[] {EntityEquipmentSlot.FEET});
 	public static final Enchantment ABLAZE = new Ablaze("ablaze", Rarity.RARE, EnumEnchantmentType.ARMOR, new EntityEquipmentSlot[] {EntityEquipmentSlot.CHEST, EntityEquipmentSlot.FEET, EntityEquipmentSlot.HEAD, EntityEquipmentSlot.LEGS});
+	public static final Enchantment AUTO_SMELTING = new AutoSmelting("auto_smelting", Rarity.UNCOMMON, EnumEnchantmentType.DIGGER, new EntityEquipmentSlot[] {EntityEquipmentSlot.MAINHAND, EntityEquipmentSlot.OFFHAND});
 	
 }
